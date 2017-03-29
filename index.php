9c590ae8aaf6c312f6a638fb19138eafc7c4416a
rgcouk <hello@rickygreen.co.uk>
2017-03-29 14:24:10 +0100

    Show Gitter
    A commit to show #timber Gitter channel

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
  * @since  SageTimber 0.1
  */
 
- ob_start();
- include ('woocommerce/cart/mini-cart.php');
-
- $context['minicart'] = ob_get_contents();
- ob_end_clean();
 
 $context = Timber::get_context();
+ob_start();
+include ('woocommerce/cart/mini-cart.php');
+$context['minicart'] = ob_get_contents();
+ob_end_clean();
 $context['posts'] = Timber::get_posts();
 $templates = array( 'pages/index.twig' );
 if ( is_home() ) {
